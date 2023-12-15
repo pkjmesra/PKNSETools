eafe525d454f1c74b105436bde88d9606c95cecf
PK <pkjmesra@gmail.com>
2023-12-16 00:03:05 +0100

    Added nse stock data fetcher

---
+++ PKNSETools/PKNSEStockDataFetcher.py
@@ -0,0 +1,126 @@
+import csv
+import random
+import warnings
+from io import StringIO
+
+warnings.simplefilter("ignore", DeprecationWarning)
+warnings.simplefilter("ignore", FutureWarning)
+import pandas as pd
+
+from PKDevTools.classes.Fetcher import fetcher
+from PKDevTools.classes.ColorText import colorText
+from PKDevTools.classes.log import default_logger
+
+# This Class Handles Fetching of Stock Data over the internet from NSE/BSE
+
+class nseStockDataFetcher(fetcher):
+                
+    def fetchNiftyCodes(self, tickerOption):
+        listStockCodes = []
+        if tickerOption == 12:
+            url = "https://archives.nseindia.com/content/equities/EQUITY_L.csv"
+            res = self.fetchURL(url)
+            if res is None or res.status_code != 200:
+                return listStockCodes
+            try:
+                data = pd.read_csv(StringIO(res.text))
+                return list(data["SYMBOL"].values)
+            except Exception as e:
+                default_logger().debug(e, exc_info=True)
+                return listStockCodes
+            
+        tickerMapping = {
+            1: "https://archives.nseindia.com/content/indices/ind_nifty50list.csv",
+            2: "https://archives.nseindia.com/content/indices/ind_niftynext50list.csv",
+            3: "https://archives.nseindia.com/content/indices/ind_nifty100list.csv",
+            4: "https://archives.nseindia.com/content/indices/ind_nifty200list.csv",
+            5: "https://archives.nseindia.com/content/indices/ind_nifty500list.csv",
+            6: "https://archives.nseindia.com/content/indices/ind_niftysmallcap50list.csv",
+            7: "https://archives.nseindia.com/content/indices/ind_niftysmallcap100list.csv",
+            8: "https://archives.nseindia.com/content/indices/ind_niftysmallcap250list.csv",
+            9: "https://archives.nseindia.com/content/indices/ind_niftymidcap50list.csv",
+            10: "https://archives.nseindia.com/content/indices/ind_niftymidcap100list.csv",
+            11: "https://archives.nseindia.com/content/indices/ind_niftymidcap150list.csv",
+            14: "https://archives.nseindia.com/content/fo/fo_mktlots.csv",
+        }
+
+        url = tickerMapping.get(tickerOption)
+
+        try:
+            res = self.fetchURL(url)
+            if res is None or res.status_code != 200:
+                return listStockCodes
+            cr = csv.reader(res.text.strip().split("\n"))
+
+            if tickerOption == 14:
+                for i in range(5):
+                    next(cr)  # skipping first line
+                for row in cr:
+                    listStockCodes.append(row[1])
+            else:
+                next(cr)  # skipping first line
+                for row in cr:
+                    listStockCodes.append(row[2])
+        except Exception as e:
+            default_logger().debug(e, exc_info=True)
+
+        return listStockCodes
+
+    # Fetch all stock codes from NSE
+    def fetchStockCodes(self, tickerOption, stockCode=None):
+        listStockCodes = []
+        if tickerOption == 0:
+            stockCode = None
+            while stockCode is None or stockCode == "":
+                stockCode = str(
+                    input(
+                        colorText.BOLD
+                        + colorText.BLUE
+                        + "[+] Enter Stock Code(s) for screening (Multiple codes should be seperated by ,): "
+                    )
+                ).upper()
+            stockCode = stockCode.replace(" ", "")
+            listStockCodes = stockCode.split(",")
+        else:
+            print(colorText.BOLD + "[+] Getting Stock Codes From NSE... ", end="")
+            listStockCodes = self.fetchNiftyCodes(tickerOption)
+            if len(listStockCodes) > 10:
+                print(
+                    colorText.GREEN
+                    + ("=> Done! Fetched %d stock codes." % len(listStockCodes))
+                    + colorText.END
+                )
+                if self.configManager.shuffleEnabled:
+                    random.shuffle(listStockCodes)
+                    print(
+                        colorText.BLUE
+                        + "[+] Stock shuffling is active."
+                        + colorText.END
+                    )
+                else:
+                    print(
+                        colorText.FAIL
+                        + "[+] Stock shuffling is inactive."
+                        + colorText.END
+                    )
+                if self.configManager.stageTwo:
+                    print(
+                        colorText.BLUE
+                        + "[+] Screening only for the stocks in Stage-2! Edit User Config to change this."
+                        + colorText.END
+                    )
+                else:
+                    print(
+                        colorText.FAIL
+                        + "[+] Screening only for the stocks in all Stages! Edit User Config to change this."
+                        + colorText.END
+                    )
+
+            else:
+                input(
+                    colorText.FAIL
+                    + "=> Error getting stock codes from NSE! Press <Enter> to exit!"
+                    + colorText.END
+                )
+
+        return listStockCodes
